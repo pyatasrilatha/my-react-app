ba5d6561f2975c24814f1217b11113c5df036a72
pyatasrilatha <69414930+pyatasrilatha@users.noreply.github.com>
2020-08-11 13:32:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
