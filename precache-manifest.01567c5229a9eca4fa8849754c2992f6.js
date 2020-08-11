91497f24323f281890d9f4ed1bce9564ef4ff9ea
Srilatha <psrilat2@LAMU02Z339SLVCG.uhc.com>
2020-08-11 14:11:46 +0530

    Updates

---
+++ precache-manifest.01567c5229a9eca4fa8849754c2992f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09326a6a358e42396e901eaba8e31bfa",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "a6efbf73bc4d3f3d3c41",
+    "url": "/my-react-app/static/css/main.049d79f2.chunk.css"
+  },
+  {
+    "revision": "74c74e1a1acc292e769c",
+    "url": "/my-react-app/static/js/2.bbd29677.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-react-app/static/js/2.bbd29677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6efbf73bc4d3f3d3c41",
+    "url": "/my-react-app/static/js/main.ed7574ff.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "c54e436892132fadbdfd68d0a4100746",
+    "url": "/my-react-app/static/media/background1.c54e4368.png"
+  },
+  {
+    "revision": "182192eb0ef387438d4e06c325364508",
+    "url": "/my-react-app/static/media/benefits.182192eb.jpg"
+  },
+  {
+    "revision": "2df07d46fe11aeda83d0f9c8e0909b58",
+    "url": "/my-react-app/static/media/corona1.2df07d46.jpeg"
+  },
+  {
+    "revision": "2a6a9eb91f2183200072730c338fd923",
+    "url": "/my-react-app/static/media/need_help.2a6a9eb9.png"
+  },
+  {
+    "revision": "b227a95390265d0b18932ec5d33b5bec",
+    "url": "/my-react-app/static/media/performance-metrics.b227a953.jpg"
+  },
+  {
+    "revision": "2975615c0ad707e314c03550a80c7399",
+    "url": "/my-react-app/static/media/tax.2975615c.jpg"
+  }
+]);
